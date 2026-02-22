090a0c25d3639c7852b45d2f7dc851da8aa70427
Kiarash <kiarash.shayegani.st@gmail.com>
2026-02-22 15:40:05 +0330

    Update filtering_1.sql

--- sql/filtering_1.sql
+++ sql/filtering_1.sql
@@ -3,3 +3,16 @@ WHERE (age BETWEEN 18 AND 22)
 AND (state IN ('Victoria','Tasmania','Queensland'))
 AND (gender <> 'n/a')
 AND (customer_name LIKE 'A%' OR customer_name LIKE 'B%'); 
+
+-- Another filtering example on customers table(subquery):
+SELECT max(counter) AS maximum_female_customers_in_a_state
+FROM
+(
+SELECT state, gender, count(customer_id) as counter
+FROM customers
+WHERE (age >= 18)
+AND (gender = 'Female')
+GROUP BY state, gender
+ORDER BY state
+) 
+AS maximum_customers;
